4b9371063c8f413efe9c591d2557fa71835ab187
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-03-07 12:58:17 +0200

    Delete Text Processing - Lab directory

--- Text Processing - Lab/02. Substring.js
+++
@@ -1,8 +0,0 @@
-function substringFinder(text, start, count) {
-    
-    let newString = text.substring(start, start + count);
-    console.log(newString);
-}
-
-substringFinder("ASentence", 1, 8);
-substringFinder("SkipWord", 4, 7);
