a0eda28133a2ae68d05bd714edb06f422fbbf196
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-03-19 09:53:45 +0200

    Tasks 3 and 4 added.
    Plain concatenations.

---
+++ Data Types and Variables - Exercise/03. Town Info.js
@@ -0,0 +1,7 @@
+function townInfo(townName, population, area) {
+    let resultString = `Town ${townName} has population of ${population} and area ${area} square km.`
+    console.log(resultString);
+}
+
+townInfo('Sofia', 1286383, 492);
+townInfo('Plovdiv', 1481353, 512);
---
+++ Data Types and Variables - Exercise/04. Convert Meters to Kilometres.js
@@ -0,0 +1,8 @@
+function convertMToKm(distance) {
+    let distanceInKm = distance / 1000;
+    let result = distanceInKm.toFixed(2);
+    console.log(result);
+}
+
+convertMToKm(798);
+convertMToKm(1852);
